6f48dfa93977f6e0bb7277defc4ed9a768dba05c
Cnbbx <admin@cnbbx.com>
2022-12-24 13:37:43 +0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -6,8 +6,8 @@
 # Author: cnbbx
 #=============================================================
 
-sed -i '$a src-git luciCnbbx https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-
 sed -i '$a src-git luciSmall https://github.com/cnbbx/luci-small' feeds.conf.default
 
 sed -i '$a src-git luciPackages https://github.com/cnbbx/luci-packages' feeds.conf.default
+
+sed -i '$a src-git kenzoPackages https://github.com/kenzok8/openwrt-packages' feeds.conf.default
